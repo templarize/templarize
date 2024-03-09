03fc8ed44fa7504dd501f5ce0838ae03cec53411
minagishl <mvxd568.co@icloud.com>
2024-03-10 01:57:00 +0900

    backup

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+import fs from 'fs';
+
+const appName = 'templarize';
+const argv: string = process.argv[2];
+
+// Get current version from package JSON
+function getVersion() {
+  fs.readFile('./package.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Failed to read package.json: ${err}`);
+      return;
+    }
+
+    const packageJson = JSON.parse(data);
+    const version = packageJson.version;
+
+    console.log(`Templarize Version: ${version}`);
+  });
+}
+
+if (argv === '--version' || argv === '-v') {
+  getVersion();
+}
+
+function help() {
+  console.log(`Usage: ${appName} [--version | -v] [--init] [--help]`);
+}
+
+if (!argv || argv === '--help') {
+  help();
+}
+
+if (argv === '--init' || argv === '-i') {
+  const dirPath = `./.${appName}`;
+
+  fs.mkdir(dirPath, (err) => {
+    if (err) {
+      console.error(`Failed to create ${appName} directory: ${err}`);
+    } else {
+      console.log(`${appName} directory created successfully.`);
+    }
+  });
+}
