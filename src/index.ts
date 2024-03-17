f5ef4d704778a9ec5af303e0cc19ea993aa6b6b7
minagishl <mvxd568.co@icloud.com>
2024-03-17 23:58:33 +0900

    Add semicolons

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-import init from './init'
-import packagesJson from '../package.json'
-import { Command } from 'commander'
+import init from './init';
+import packagesJson from '../package.json';
+import { Command } from 'commander';
 
-const program = new Command()
-const appName = packagesJson.name ?? 'templarize'
+const program = new Command();
+const appName = packagesJson.name ?? 'templarize';
 
 // Get current version
 program.version(
   `${appName.charAt(0).toUpperCase() + appName.slice(1)} version ${packagesJson.version}`,
   '-v, --version',
   `Get current version of ${appName}`
-)
+);
 
 // Show help
-program.helpOption('-h, --help', 'Show help for commands')
+program.helpOption('-h, --help', 'Show help for commands');
 
 // Options
 program
   .option('-d, --debug', 'Output extra debugging')
-  .option('-g, --global', 'Create under user folder')
+  .option('-g, --global', 'Create under user folder');
 
 program
   .command('init')
   .description(`Initialize ${appName}`)
   .action(() => {
-    init(appName, Boolean(program.opts().global))
-  })
+    init(appName, Boolean(program.opts().global));
+  });
 
-program.parse()
+program.parse();
--- src/init.ts
+++ src/init.ts
@@ -1,26 +1,26 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'fs';
+import path from 'path';
 
 export default function init(appName: string, global: boolean): void {
-  let dirPath: string
-  const homeDir: string = process.env.HOME ?? process.env.USERPROFILE ?? ''
+  let dirPath: string;
+  const homeDir: string = process.env.HOME ?? process.env.USERPROFILE ?? '';
 
   if (global) {
-    dirPath = path.join(homeDir, `.${appName}`)
+    dirPath = path.join(homeDir, `.${appName}`);
   } else {
-    dirPath = `./.${appName}`
+    dirPath = `./.${appName}`;
   }
 
   if (fs.existsSync(dirPath)) {
-    console.error(`${appName} directory already exists.`)
-    return
+    console.error(`${appName} directory already exists.`);
+    return;
   }
 
   fs.mkdir(dirPath, (err: any) => {
     if (err !== null) {
-      console.error(`Failed to create ${appName} directory: ${err}`)
+      console.error(`Failed to create ${appName} directory: ${err}`);
     } else {
-      console.log(`${appName} directory created successfully.`)
+      console.log(`${appName} directory created successfully.`);
     }
-  })
+  });
 }
