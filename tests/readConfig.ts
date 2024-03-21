b11f8aa0c60daea3d09ddbd0ded7238bca38910f
minagishl <mvxd568.co@icloud.com>
2024-03-21 17:54:30 +0900

    Add readConfig.ts function

---
+++ tests/readConfig.ts
@@ -0,0 +1,12 @@
+import readConfig from '~/readConfig';
+import { test } from 'vitest';
+import path from 'path';
+
+test('reads config', async ({ expect }) => {
+  const configDir = path.join(process.cwd(), 'tests', 'example');
+  const configs = await readConfig(configDir);
+  expect(configs).toEqual([
+    { name: 'component', description: 'Create a new component' },
+    { name: 'component-dev', description: 'Create a new component (dev)' },
+  ]);
+});
