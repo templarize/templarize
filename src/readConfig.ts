2e77b6000b335bf989e8c42f72a6c921ddd18123
みなぎし <114616680+minagishl@users.noreply.github.com>
2024-04-17 01:12:59 +0900

    Revert "build: update prettier settings"
    This reverts commit 75b8dd03b2dcb1fec2123f677ac53536e8c741e3.

--- src/readConfig.ts
+++ src/readConfig.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export default async function readConfig(
-  dir: string,
+  dir: string
 ): Promise<Array<{ name: string; description: string }>> {
   const result: Array<{ name: string; description: string }> = [];
   const files = fs.readdirSync(dir);
@@ -11,21 +11,12 @@ export default async function readConfig(
     const filePath = path.join(dir, file);
     if (fs.statSync(filePath).isDirectory()) {
       const configFile = path.join(filePath, 'config');
-      const supportedExtensions = [
-        '.ts',
-        '.js',
-        '.mts',
-        '.cts',
-        '.mjs',
-        '.cjs',
-      ];
+      const supportedExtensions = ['.ts', '.js', '.mts', '.cts', '.mjs', '.cjs'];
 
       for (const extension of supportedExtensions) {
         const configFilePath = `${configFile}${extension}`;
         if (fs.existsSync(configFilePath)) {
-          const { default: config } = await import(
-            path.join(process.cwd(), configFilePath)
-          );
+          const { default: config } = await import(path.join(process.cwd(), configFilePath));
           const name = path.basename(path.dirname(configFilePath));
           result.push({
             name: name ?? '',
