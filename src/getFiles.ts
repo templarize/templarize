4df067fc0140bcc02d263fef156c16bcbfa7722e
minagishl <mvxd568.co@icloud.com>
2024-04-17 01:00:15 +0900

    build: update prettier settings

--- src/getFiles.ts
+++ src/getFiles.ts
@@ -1,7 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 
-export default async function getFiles(dirPath: string, result: string[]): Promise<string[]> {
+export default async function getFiles(
+  dirPath: string,
+  result: string[],
+): Promise<string[]> {
   try {
     const files = await fs.promises.readdir(dirPath, { withFileTypes: true });
     for (const file of files) {
